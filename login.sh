8feacad5e187a63f43a4dfa63ae15b1863899a1e
Clif Wolfe <clifwolfe1@gmail.com>
2019-10-23 12:42:41 -0400

    log output from login

--- login.sh
+++ login.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+
 adb root > /dev/null
 adb shell am start -n com.badguy.terrortime/com.badguy.terrortime.LoginActivity
+if [ $# -eq 1 ]; then
+	sleep 2
+	nohup adb logcat --pid=$(adb shell pgrep terror) \*:D > $1 & disown
+fi
 adb shell input text "roberto--vhost-385@terrortime.app"
 adb shell input keyevent 61
 adb shell input text "412270"
